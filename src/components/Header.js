53d8d3e78f3d0a7b18b4db36715f3aae6a0ac223
Siarhei1927 <shaurylau@gmail.com>
2023-10-09 13:12:02 +0200

    feat: add header nav and styles for it

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,12 @@ export default function Header() {
     <header>
       <div>
         <span className='logo'>Szmat sala</span>
+        <ul className='nav'>
+          <li>Catalog</li>
+          <li>About us</li>
+          <li>Your profile</li>
+          <li>Basket</li>
+        </ul>
       </div>
       <div className='presentation'></div>
     </header>
